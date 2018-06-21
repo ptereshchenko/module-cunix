18b5c768f2fed2e4802deb242bcc19645a7eef3e
Polina <polatka3000@gmail.com>
2018-06-21 23:31:34 +0300

    2nd ex

---
+++ ex02/src/my_strcmp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int my_strcmp(char *str1, char *str2)
+{
+    int i;
+
+    i = 0;
+    while (str1[i] != '\0')
+    {
+        if (str1[i] < str2[i])
+        {
+           return -1;
+        } else if (str1[i] > str2[i])
+        {
+           return 1;
+        }
+        i++;
+    }
+       if (str2[i] != '\0')
+       {
+         return -1;
+       }
+       else
+       {
+       return 0;
+       }
+
+}
